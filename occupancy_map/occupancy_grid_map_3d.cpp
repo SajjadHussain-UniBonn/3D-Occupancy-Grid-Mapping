90d671fd839ff32b54247b980e20261af40f37d5
SajjadHussain-UniBonn <sajjadravianuetian@gmail.com>
2025-07-26 02:41:51 +0200

    added cmake code for Occupancy grid map library.

--- occupancy_map/occupancy_grid_map_3d.cpp
+++ occupancy_map/occupancy_grid_map_3d.cpp
@@ -1,5 +1,6 @@
 #include "occupancy_grid_map_3d.hpp"
 
+
 OccupancyGridMap3D::OccupancyGridMap3D(const double voxel_size) : voxel_size(voxel_size) {}
 Eigen::Vector3i OccupancyGridMap3D::pointToVoxel(const Eigen::Vector3d& point) const{
     return (point/voxel_size).array().floor().cast<int>();}
