fedfbbb6063d3ff8aeea08ff7e764ad8ee1651ca
SajjadHussain-UniBonn <sajjadravianuetian@gmail.com>
2025-08-05 06:31:39 +0200

    Changed Cmake as per new code. and added final code to log scan time etc.

--- app/main.cpp
+++ app/main.cpp
@@ -1,29 +1,34 @@
 #include "dataloader.hpp"
 #include "visualizer.hpp"
-#include "open3d/Open3D.h"
-#include <Eigen/Core>
-#include <iostream>
-#include <string>
-#include <utility>
-#include <vector>
 #include "occupancy_grid_map_3d.hpp"
-
-
-using Vector3dVector = std::vector<Eigen::Vector3d>;
-using PoseAndCloud = std::pair<Eigen::Matrix4d,Vector3dVector>;
+#include <chrono>
+#include <iostream>
+#include <iomanip>
 
 int main()
 {
-    OccupancyGridMap3D map(0.2);
     const std::string data_dir = "/home/sajjad/3D-Occupancy-Grid-Mapping/data";
     const dataloader::Dataset dataset = dataloader::Dataset(data_dir);
-    const PoseAndCloud pose_and_cloud = dataset[10];
-    
-    Eigen::Matrix4d pose = pose_and_cloud.first;
-    std::vector<Eigen::Vector3d> points = pose_and_cloud.second;
+    OccupancyGridMap3D map(0.2);
+    double total_scan_time = 0;
+    auto t0 = std::chrono::high_resolution_clock::now();
+    std::cout<<dataset.size()<<std::endl;
+    for (size_t i=0; i<dataset.size(); ++i)
+    {
+        auto [pose,cloud] = dataset[i];
+        auto start =  std::chrono::high_resolution_clock::now();
+        map.integrateScan(pose,cloud);
+        auto end = std::chrono::high_resolution_clock::now();
+        total_scan_time += std::chrono::duration<double>(end - start).count();
 
-    map.integrateScan(pose, points);
-    std::vector<Eigen::Vector3d> occupied_points = map.extractFreePoints();
+        double progress = (static_cast<double>(i+1) / dataset.size()) * 100.0;
+        std::cout << "\rProgress: " << std::fixed << std::setprecision(1) << progress << "% completed" << std::flush;
+    }    
+    auto t1 = std::chrono::high_resolution_clock::now();
+    std::cout << "\nAverage scan time: " << total_scan_time / dataset.size() << " s\n";
+    std::cout << "Total execution time: " << std::chrono::duration<double>(t1 - t0).count() << " s\n";
+    visualizeOccupancyMap(map.extractOccupiedVoxels(), map.extractFreeVoxels());
+
     return 0;
 }
 
