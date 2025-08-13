180f7601c99982c06d9cf9fd29688c748b6f68cc
SajjadHussain-UniBonn <sajjadravianuetian@gmail.com>
2025-08-13 02:58:35 +0200

    Deleted visualizeOccupancyGripMap.

--- visualizer/visualizer.hpp
+++ visualizer/visualizer.hpp
@@ -10,19 +10,3 @@ inline void visualize(
     open3d::visualization::DrawGeometries(
             {std::make_shared<open3d::geometry::PointCloud>(pointcloud)});
 }
-inline void visualizeOccupancyMap(const std::vector<Eigen::Vector3d>& occupied_points,
-                                     const std::vector<Eigen::Vector3d>& free_points) 
-{
-    auto cloud = std::make_shared<open3d::geometry::PointCloud>();
-    for (const auto& p : occupied_points) 
-    {
-        cloud->points_.emplace_back(p);
-        cloud->colors_.emplace_back(Eigen::Vector3d(1.0, 0.0, 0.0));
-    }
-    for (const auto& p : free_points) 
-    {
-        cloud->points_.emplace_back(p);
-        cloud->colors_.emplace_back(Eigen::Vector3d(0.0, 1.0, 0.0));
-    }
-    open3d::visualization::DrawGeometries({cloud});
-}
